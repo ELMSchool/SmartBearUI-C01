1dc7c2c881c01dd7957068c4dd83776bc1e66749
Farid Ahmadov <faridahmadov@mail.com>
2023-03-05 14:14:28 -0500

    We did changes in loginpage and test page

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,31 @@
 package pages;
 
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.Driver;
+
 public class LoginPage {
+	public LoginPage() {
+
+		PageFactory.initElements(Driver.getDriver(), this);
+
+	}
+//	@FindBy(id="userName")
+//	public WebElement fullNameInputBox;
+	////ctl00_MainContent_login_button
+	
+	@FindBy (id="ctl00_MainContent_username")
+	public WebElement userNameInput;
+	
+	@FindBy (id="ctl00_MainContent_password")
+	public WebElement userPasswordInput;
+	
+	@FindBy (id="ctl00_MainContent_login_button")
+	public WebElement loginButton;
+	
+	
 
 }
--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -1,11 +1,27 @@
 package tests;
 
+import pages.LoginPage;
 import utils.ConfigReader;
 
+import utils.Driver;
+
 public class Test {
-	
+
 	public static void main(String[] args) {
-		System.out.println(ConfigReader.getPropertyValue("url"));
+		LoginPage credential = new LoginPage();
+		
+		
+		Driver.getDriver().get(ConfigReader.getPropertyValue("url"));
+		//2.We created object of Loginpage and we stored login input box location inside of Loginpage, and we reached out that 
+		//that webelement by using id of that element.We sendKeys to enter the username
+		credential.userNameInput.sendKeys("Tester");
+		//3.entering password
+		credential.userPasswordInput.sendKeys("test");
+		//4. clicking loging button
+		credential.loginButton.click();
+		
+
+		
 	}
 
 }
