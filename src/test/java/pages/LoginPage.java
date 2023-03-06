a1193d4b9334799fc89b76f6edb1486c03348418
Fuad Suleyman <fuad.msuleyman@gmail.com>
2023-03-05 19:18:18 -0500

    test

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,15 @@
 package pages;
 
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utils.Driver;
+
 public class LoginPage {
+	
 
 }
--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -5,7 +5,9 @@ import utils.ConfigReader;
 public class Test {
 	
 	public static void main(String[] args) {
+		
 		System.out.println(ConfigReader.getPropertyValue("url"));
+		
 	}
 
 }
